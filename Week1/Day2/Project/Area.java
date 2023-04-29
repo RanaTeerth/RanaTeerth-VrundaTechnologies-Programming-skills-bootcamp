4a2c0a878a5f35934494eea3098b89596b3cd67e
Ishan3450 <jaganiishan3450@duck.com>
2023-04-29 11:01:22 +0530

    Revised some code of Week1

--- Week1/Day2/Project/Area.java
+++ Week1/Day2/Project/Area.java
@@ -5,9 +5,22 @@
 package Week1.Day2.Project;
 
 public class Area{
-    public static void main(String[] args) {
-        int height = 30, width=12;
+    public static void area(int length){
+        System.out.println("The area of square is " + (length * length));
+    }
 
-        System.out.println(height * width);
+    public static void area(int length, int width){
+        System.out.println("The are of rectangle is " + (length * width));
+    }
+
+    public static void area(double radius){
+        System.out.println("The area of circle is " + (3.14 * radius * radius));
+    }
+
+    public static void main(String[] args) {
+        area(15); // 225
+        area(10,15); // 150
+        area(2.124); // 14.16572064...
+
     }
 }
--- Week1/Day2/Project/PositiveOrNegative.java
+++ Week1/Day2/Project/PositiveOrNegative.java
@@ -4,10 +4,10 @@
 
 package Week1.Day2.Project;
 
+import java.util.Scanner;
+
 public class PositiveOrNegative {
-    public static void main(String[] args){
-        int num = 0;
-        
+    public static void getAns(int num){
         if(num > 0){ // num is positive
             System.out.println(num + " is positive. ");
         } else if(num < 0){ // num is negative
@@ -15,5 +15,15 @@ public class PositiveOrNegative {
         } else{ // num is 0
             System.out.println("Number is zero. ");
         }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num = 0;
+        
+        System.out.print("Enter any int range number: ");
+        num = sc.nextInt();
+
+        getAns(num);
     }
 }
