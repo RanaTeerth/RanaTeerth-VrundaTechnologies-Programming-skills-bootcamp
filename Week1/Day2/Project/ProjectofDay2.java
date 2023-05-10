24da060fde389cacd657f0ad754ce95701c56595
RanaTeerth <124882422+RanaTeerth@users.noreply.github.com>
2023-05-10 10:49:13 +0530

    Update ProjectofDay2.java

--- Week1/Day2/Project/ProjectofDay2.java
+++ Week1/Day2/Project/ProjectofDay2.java
@@ -1,19 +1,17 @@
-package Week1.Day2.Project;
-
-class JavaProjectSolution {
+class ProjectofDay2 {   //Create a class named ProjectofDay2 
     public static void main(String[] args) {
-        String email = "demo@email.com";
-        String password = "123";
-        String userEmail = "demo1@email.com";
-        String userPassword = "234";
-        if(email.equals(userEmail)){
-            if(password.equals(userPassword)){
-                System.out.println("Success");
+        String email1 = "teerth@email.com";  //Store email
+        String password1 = "a1b2c3";         //store password
+        String userEmail = "test@email.com";   //store userEmail
+        String userPassword = "xyz";           //Store userPassword
+        if(email.equals(userEmail)){           //compare email1 and useremail
+            if(password.equals(userPassword)){ //if email1 and useremail is matched then compare password1 and userPassword
+                System.out.println("Success"); //if both email and password match then print Success
             }else{
-                System.out.println("Wrong Credentials");
+                System.out.println("Wrong Credentials"); //if both email matched but password not matched then print Wrong Credentials.
             }
         }else{
-            System.out.println("Wrong Credentials");
+            System.out.println("Wrong Credentials");  //if both email not matched then print Wrong Credentials
         }
     }
 }
